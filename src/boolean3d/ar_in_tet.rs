090141b7cd3e3636cb29fd4d85ec2f57d5829e12
Kaizheng Liu <lkzheg@gmail.com>
2024-10-29 00:29:44 +0800

    Refactor det4.rs for improved code organization and performance

--- src/boolean3d/ar_in_tet.rs
+++ src/boolean3d/ar_in_tet.rs
@@ -3,13 +3,48 @@ use std::{
     cell::LazyCell,
 };
 
-use crate::mesh::{clone_vec_in, FaceId, Mesh, SurfaceMesh};
+use crate::{
+    mesh::{clone_vec_in, FaceId, Mesh, SurfaceMesh},
+    predicates::{det4, sign_reverse, sign_reversed, Orientation},
+};
 
 pub(super) struct Arrangement<A: Allocator + Copy> {
     mesh: SurfaceMesh<A>,
-    vertices: Vec<[FaceId; 3], A>,
-    edges: Vec<[FaceId; 2], A>,
+    vertices: Vec<[usize; 3], A>,
+    edges: Vec<[usize; 2], A>,
     planes: Vec<[f64; 4], A>,
+}
+
+fn orient3d<A: Allocator + Copy>(
+    pa: &[f64; 4],
+    pb: &[f64; 4],
+    pc: &[f64; 4],
+    pd: &[f64; 4],
+    alloc: A,
+) -> Orientation {
+
+    #[rustfmt::skip]    
+    let numerator_sign = det4(
+        pa[0], pa[1], pa[2], pa[3], 
+        pb[0], pb[1], pb[2], pb[3], 
+        pc[0], pc[1], pc[2], pc[3], 
+        pd[0], pd[1], pd[2], pd[3], 
+        alloc,
+    );
+    let denominator_sign = det4(
+        pa[0], pa[1], pa[2], pa[3], 
+        pb[0], pb[1], pb[2], pb[3], 
+        pc[0], pc[1], pc[2], pc[3], 
+        1.0  , 1.0  , 1.0  ,   1.0,
+        alloc,
+    );
+
+    if denominator_sign == Orientation::Positive {
+        return numerator_sign;
+    } else {
+        debug_assert!(denominator_sign != Orientation::Zero);
+        return sign_reverse(numerator_sign);
+    }
 }
 
 impl<A: Allocator + Copy> Arrangement<A> {
@@ -17,23 +52,23 @@ impl<A: Allocator + Copy> Arrangement<A> {
         let mesh = SurfaceMesh::new([[1, 3, 2], [0, 2, 3], [0, 3, 1], [0, 1, 2]], alloc);
         let mut vertices = Vec::with_capacity_in(4, alloc);
         vertices.extend(mesh.vertices().map(|v| {
-            let mut res = [FaceId::default(); 3];
+            let mut res = [0; 3];
             for (fid, hid) in res.iter_mut().zip(v.incoming_halfedges()) {
-                *fid = mesh.he_face(*hid);
+                *fid = mesh.he_face(*hid).0;
             }
             res
         }));
 
         let mut edges = Vec::with_capacity_in(6, alloc);
         edges.extend(mesh.edges().map(|e| {
-            let mut res = [FaceId::default(); 2];
+            let mut res = [0; 2];
             for (fid, hid) in res.iter_mut().zip(e.halfedges()) {
-                *fid = mesh.he_face(*hid);
+                *fid = mesh.he_face(*hid).0;
             }
             res
         }));
 
-        let mut planes = Vec::with_capacity_in(3, alloc);
+        let mut planes = Vec::with_capacity_in(4, alloc);
         planes.push([1.0, 0.0, 0.0, 0.0]);
         planes.push([0.0, 1.0, 0.0, 0.0]);
         planes.push([0.0, 0.0, 1.0, 0.0]);
@@ -55,9 +90,35 @@ impl<A: Allocator + Copy> Arrangement<A> {
             planes: clone_vec_in(&self.planes, alloc),
         }
     }
+
+    fn add_plane<A1: Allocator + Copy>(&mut self, plane: &[f64; 4], alloc: A1) {
+        let pid = self.planes.len();
+        self.planes.push(plane.clone());
+        let mut vert_orientations = Vec::with_capacity_in(self.mesh.n_vertices_capacity(), alloc);
+        vert_orientations.extend(self.mesh.vertices().map(|v| {
+            let vid = *v;
+            let v_planes = &self.vertices[vid];
+            orient3d(&self.planes[v_planes[0]], &self.planes[v_planes[1]],  &self.planes[v_planes[2]], plane, alloc)
+        }));
+
+        let n_old_edges = self.mesh.n_edges_capacity();
+        for eid in 0..n_old_edges {
+            let eid = eid.into();
+            let [va, vb] = self.mesh.e_vertices(eid);
+            if sign_reversed(vert_orientations[va], vert_orientations[vb]) {
+                self.mesh.split_edge(eid, alloc);
+                let e_planes = &self.edges[eid];
+                self.vertices.push([e_planes[0], e_planes[1], pid]);
+                self.edges.push(e_planes.clone());
+            }
+        }
+    }
 }
 
 pub(super) fn arrangement_for_tet<A: Allocator + Copy>(planes: &[[f64; 4]], alloc: A) {
     let one_tet = LazyCell::new(|| Arrangement::new_tet(Global));
-    let arrangement = one_tet.clone_in(alloc);
+    let mut ar = one_tet.clone_in(Global);
+    for plane in planes {
+        ar.add_plane(plane, alloc);
+    }
 }
---
+++ src/predicates/det4.rs
@@ -0,0 +1,169 @@
+use std::alloc::Allocator;
+
+use super::{
+    abs_max, double_to_sign, dummy_abs_max, ExpansionNum, GenericNum, IntervalNumber, Orientation,
+};
+
+pub fn det4<A: Allocator + Copy>(
+    a: f64,
+    b: f64,
+    c: f64,
+    d: f64,
+    e: f64,
+    f: f64,
+    g: f64,
+    h: f64,
+    i: f64,
+    j: f64,
+    k: f64,
+    l: f64,
+    m: f64,
+    n: f64,
+    o: f64,
+    p: f64,
+    alloc: A,
+) -> Orientation {
+    {
+        // static filter
+        let (det, max_var) =
+            det4_impl::<true, _, _>(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, abs_max);
+        let mut epsilon = max_var.unwrap();
+
+        epsilon *= epsilon;
+        epsilon *= epsilon;
+        epsilon *= 1.953992523340277e-14;
+        if det.abs() > epsilon {
+            return double_to_sign(det);
+        }
+    }
+    {
+        // dynamic filter
+        let (det, _) = det4_impl::<false, IntervalNumber, _>(
+            a.into(),
+            b.into(),
+            c.into(),
+            d.into(),
+            e.into(),
+            f.into(),
+            g.into(),
+            h.into(),
+            i.into(),
+            j.into(),
+            k.into(),
+            l.into(),
+            m.into(),
+            n.into(),
+            o.into(),
+            p.into(),
+            dummy_abs_max,
+        );
+        if det.not_zero() {
+            if det.positive() {
+                return Orientation::Positive;
+            } else {
+                return Orientation::Negative;
+            }
+        }
+    }
+    {
+        // exact
+        let (det, _) = det4_impl::<false, ExpansionNum<A>, _>(
+            [a].to_vec_in(alloc).into(),
+            [b].to_vec_in(alloc).into(),
+            [c].to_vec_in(alloc).into(),
+            [d].to_vec_in(alloc).into(),
+            [e].to_vec_in(alloc).into(),
+            [f].to_vec_in(alloc).into(),
+            [g].to_vec_in(alloc).into(),
+            [h].to_vec_in(alloc).into(),
+            [i].to_vec_in(alloc).into(),
+            [j].to_vec_in(alloc).into(),
+            [k].to_vec_in(alloc).into(),
+            [l].to_vec_in(alloc).into(),
+            [m].to_vec_in(alloc).into(),
+            [n].to_vec_in(alloc).into(),
+            [o].to_vec_in(alloc).into(),
+            [p].to_vec_in(alloc).into(),
+            dummy_abs_max,
+        );
+        return double_to_sign(*det.vec.last().unwrap());
+    }
+}
+
+fn det4_impl<const NEED_MAX: bool, T: GenericNum, F>(
+    a: T,
+    b: T,
+    c: T,
+    d: T,
+    e: T,
+    f: T,
+    g: T,
+    h: T,
+    i: T,
+    j: T,
+    k: T,
+    l: T,
+    m: T,
+    n: T,
+    o: T,
+    p: T,
+    abs_max: F,
+) -> (T, Option<T>)
+where
+    F: FnOnce(&[T]) -> Option<T>,
+{
+    let af = &a * &f;
+    let be = &b * &e;
+    let kp = &k * &p;
+    let lo = &l * &o;
+    let ce = &c * &e;
+    let ag = &a * &g;
+    let jp = &j * &p;
+    let ln = &l * &n;
+    let ah = &a * &h;
+    let de = &d * &e;
+    let jo = &j * &o;
+    let kn = &k * &n;
+    let bg = &b * &g;
+    let cf = &c * &f;
+    let ip = &i * &p;
+    let lm = &l * &m;
+    let df = &d * &f;
+    let bh = &b * &h;
+    let io = &i * &o;
+    let km = &k * &m;
+    let ch = &c * &h;
+    let dg = &d * &g;
+    let in_ = &i * &n;
+    let jm = &j * &m;
+    let d1 = af - be;
+    let d2 = kp - lo;
+    let d3 = ce - ag;
+    let d4 = jp - ln;
+    let d5 = ah - de;
+    let d6 = jo - kn;
+    let d7 = bg - cf;
+    let d8 = ip - lm;
+    let d9 = df - bh;
+    let d10 = io - km;
+    let d11 = ch - dg;
+    let d12 = in_ - jm;
+    let t1 = d1 * d2;
+    let t2 = d3 * d4;
+    let t3 = d5 * d6;
+    let t4 = d7 * d8;
+    let t5 = d9 * d10;
+    let t6 = d11 * d12;
+    let r12 = t1 + t2;
+    let r34 = t3 + t4;
+    let r56 = t5 + t6;
+    let r1234 = r12 + r34;
+    let r123456 = r1234 + r56;
+
+    let max_var = if NEED_MAX {
+        abs_max(&[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p])
+    } else {
+        None
+    };
+    (r123456, max_var)
+}
